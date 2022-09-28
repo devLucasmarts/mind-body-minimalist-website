61641fc25ba817d608dfbd1aa8b9a92cabbd7916
martsDev <martssilvapro@gmail.com>
2022-09-28 15:28:58 -0300

    implements video play, pause and animations

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,21 @@ burger.addEventListener('click', () => {
 
     burger.classList.toggle('active');
 });
+
+const videos = gsap.utils.toArray('.video');
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+    ScrollTrigger.create({
+        trigger: video,
+        start: 'top center',
+        end: 'bottom center',
+        onEnter: () => {
+            gsap.to(video, { opacity: 1 });
+            video.play();
+        },
+        onEnterBack: () => video.play(),
+        onLeave: () => video.pause(),
+        onLeaveBack: () => video.pause(),
+    });
+});
